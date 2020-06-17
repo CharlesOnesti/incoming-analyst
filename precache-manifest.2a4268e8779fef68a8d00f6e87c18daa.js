4d664ae3366237abf47f7fe42c2b1a2f7256b358
Charles Onesti <charles@Caligula.home>
2020-06-17 16:10:07 -0400

    Updates

---
+++ precache-manifest.2a4268e8779fef68a8d00f6e87c18daa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc59e45b789bd99b438b87f251d21a36",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b71c21c8a371c2c7f7c",
+    "url": "/portfolio/static/css/main.eb19a952.chunk.css"
+  },
+  {
+    "revision": "560dfdb53081e7794ed3",
+    "url": "/portfolio/static/js/2.4744dced.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/portfolio/static/js/2.4744dced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b71c21c8a371c2c7f7c",
+    "url": "/portfolio/static/js/main.f77c25f3.chunk.js"
+  },
+  {
+    "revision": "7363d80720fefcf1c18c",
+    "url": "/portfolio/static/js/runtime-main.2c618ecd.js"
+  },
+  {
+    "revision": "7c481b8c7b353e06fd8af8c4d31445e6",
+    "url": "/portfolio/static/media/decor2.7c481b8c.png"
+  },
+  {
+    "revision": "219a368460b4547ef147f8f8322bd643",
+    "url": "/portfolio/static/media/owllogo.219a3684.png"
+  },
+  {
+    "revision": "60db1f873b88088593231303bb34bec7",
+    "url": "/portfolio/static/media/profilepic.60db1f87.png"
+  }
+]);
