e816d9061b21f1508e94152c30d1c20a8b9cc814
Charles Onesti <charles@Caligula.home>
2020-06-17 15:19:37 -0400

    Updates

--- precache-manifest.ecdd147b4695d20bddd51bb9a30dc270.js
+++ precache-manifest.ecdd147b4695d20bddd51bb9a30dc270.js
@@ -1,38 +1,40 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-underscore-dangle */
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b62ab2366f6223285d71a67b3414ebe9",
-    "url": "/Portfolio/index.html"
+    revision: 'b62ab2366f6223285d71a67b3414ebe9',
+    url: '/Portfolio/index.html',
   },
   {
-    "revision": "37a5313975dcb503011b",
-    "url": "/Portfolio/static/css/main.eb19a952.chunk.css"
+    revision: '37a5313975dcb503011b',
+    url: '/Portfolio/static/css/main.eb19a952.chunk.css',
   },
   {
-    "revision": "6ddfff8c2ac2c3d121b8",
-    "url": "/Portfolio/static/js/2.c71782dd.chunk.js"
+    revision: '6ddfff8c2ac2c3d121b8',
+    url: '/Portfolio/static/js/2.c71782dd.chunk.js',
   },
   {
-    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
-    "url": "/Portfolio/static/js/2.c71782dd.chunk.js.LICENSE.txt"
+    revision: '882fee5ad56cfd78b1cc5d94b617aa42',
+    url: '/Portfolio/static/js/2.c71782dd.chunk.js.LICENSE.txt',
   },
   {
-    "revision": "37a5313975dcb503011b",
-    "url": "/Portfolio/static/js/main.76b893f9.chunk.js"
+    revision: '37a5313975dcb503011b',
+    url: '/Portfolio/static/js/main.76b893f9.chunk.js',
   },
   {
-    "revision": "5a59fc6085c1cf3823e7",
-    "url": "/Portfolio/static/js/runtime-main.ba1094b7.js"
+    revision: '5a59fc6085c1cf3823e7',
+    url: '/Portfolio/static/js/runtime-main.ba1094b7.js',
   },
   {
-    "revision": "7c481b8c7b353e06fd8af8c4d31445e6",
-    "url": "/Portfolio/static/media/decor2.7c481b8c.png"
+    revision: '7c481b8c7b353e06fd8af8c4d31445e6',
+    url: '/Portfolio/static/media/decor2.7c481b8c.png',
   },
   {
-    "revision": "219a368460b4547ef147f8f8322bd643",
-    "url": "/Portfolio/static/media/owllogo.219a3684.png"
+    revision: '219a368460b4547ef147f8f8322bd643',
+    url: '/Portfolio/static/media/owllogo.219a3684.png',
   },
   {
-    "revision": "60db1f873b88088593231303bb34bec7",
-    "url": "/Portfolio/static/media/profilepic.60db1f87.png"
-  }
-]);
+    revision: '60db1f873b88088593231303bb34bec7',
+    url: '/Portfolio/static/media/profilepic.60db1f87.png',
+  },
+])
