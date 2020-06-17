a58a2c9fb3d9db875562654f66bf8224859f87b7
Charles Onesti <charles@Caligula.home>
2020-06-17 14:07:13 -0400

    Updates

---
+++ precache-manifest.2939fe5ba34f8c220d3373c06f73e819.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075cb171da3aa51d221442920620d57d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba27b79082f00fe1099d",
+    "url": "/static/css/main.eb19a952.chunk.css"
+  },
+  {
+    "revision": "161d667081e43f90c7d5",
+    "url": "/static/js/2.4b181ae6.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.4b181ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba27b79082f00fe1099d",
+    "url": "/static/js/main.ab9f5a55.chunk.js"
+  },
+  {
+    "revision": "70238dabd126889a7446",
+    "url": "/static/js/runtime-main.1e2782f1.js"
+  },
+  {
+    "revision": "7c481b8c7b353e06fd8af8c4d31445e6",
+    "url": "/static/media/decor2.7c481b8c.png"
+  },
+  {
+    "revision": "219a368460b4547ef147f8f8322bd643",
+    "url": "/static/media/owllogo.219a3684.png"
+  },
+  {
+    "revision": "60db1f873b88088593231303bb34bec7",
+    "url": "/static/media/profilepic.60db1f87.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.f307687fa146ddc98e7c37ed9e9b88e4.js"
+  "/precache-manifest.2939fe5ba34f8c220d3373c06f73e819.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
