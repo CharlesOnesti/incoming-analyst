b0d20177105cf919e9f3f9136080413052c78765
unknown <chrisonesti@gmail.com>
2020-07-21 23:30:56 -0400

    welcome paragraph

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import HeaderText from '../../components/HeaderText'
+import WelcomeParagraph from './styles'
+
 
 const Home = () => {
   console.log('home rendered')
   return (
     <>
       <HeaderText text="Coming Soon" color1="black" />
+      <WelcomeParagraph>
+        "Hello World"
+      </WelcomeParagraph>
     </>
   )
 }
-
 
 export default Home
