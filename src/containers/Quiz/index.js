f9d1666f3ad261d79b001c6db643da2cc52f1693
unknown <chrisonesti@gmail.com>
2020-07-24 23:46:05 -0400

    Quiz Page Text Style

--- src/containers/Quiz/index.js
+++ src/containers/Quiz/index.js
@@ -13,28 +13,35 @@ const Quiz = () => {
   return (
     <Main>
       <HeaderText text="Paper LBO Tool" color1="black" />
-
+      <br />
       {!submitted ? (
         <>
           <p>
             You are tasked with evaluating an investment opportunity in Company A,
-            which has an LTM Revenue of [LTM_Rev] and an EBITDA Margin of [EBITDA_margin] percent.
-
+            which has an LTM Revenue of $[LTM_Rev] million and an EBITDA Margin of [EBITDA_margin] percent.
+            <br />
+            <br />
             Your firm is considering to fund the acquisition with [Debt_percent] percent debt with
             an interest rate of [Int_rate].
-
+            <br />
+            <br />
             The management is confident that the firm can acheive a growth of [Rev_growth] per year
             and maintain constant EBITDA margins.
-
+            <br />
+            <br />
             The firm has a tax rate of [Tax_rate].
-
-            Capital Expenditures are equal to 10 percent of the revenue and are equal to
+            <br />
+            <br />
+            Capital Expenditures are equal to [CapEx] percent of the revenue and are equal to
             Depreciation
-
+            <br />
+            <br />
             Net Working Capital is [NWC_per] percent of revenue each year
-
+            <br />
+            <br />
             What would the IRR of this investment be over a 5 year horizon?
           </p>
+          <br />
           <form onSubmit={handleSubmit}>
             <input
               type="text"
--- src/containers/Quiz/styles.js
+++ src/containers/Quiz/styles.js
@@ -3,4 +3,6 @@ import styled from 'styled-components'
 export const Main = styled.div`
   margin-left: 10%;
   margin-right: 10%;
+  font-size: 20px;
+  text-align: left;
 `
